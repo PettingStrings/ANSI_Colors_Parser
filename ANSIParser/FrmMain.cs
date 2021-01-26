38f61c4913fa36e3d88e3912503eb9c3f6b30c61
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2021-01-26 11:16:49 +0100

    Added little example

--- ANSIParser/FrmMain.cs
+++ ANSIParser/FrmMain.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Nothing
 namespace ANSIParser
 {//WORK IN PROGRESS
     public partial class FrmMain : Form
